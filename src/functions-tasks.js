90862da1f017ca7bf69e62a5407470e891cfa3b3
Maria Treier <mariatraier@gmail.com>
2024-04-11 05:08:01 +0200

    feat: complete part of the task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -241,8 +241,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom - 1;
+  return function fn() {
+    counter += 1;
+    return counter;
+  };
 }
 
 module.exports = {
